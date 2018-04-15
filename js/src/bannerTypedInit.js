4f5d104776d64334c070c1bdd276eac74d888e46
lopu <ireply@myleisure.com.au>
2018-04-16 00:52:58 +1000

    Ready for show

--- js/src/bannerTypedInit.js
+++ js/src/bannerTypedInit.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(".intro-text").typed({
-        // strings: ["Welcome to the revolution,^700 \n we'll help work out what LEDs you need."],
+        // strings: ["Welcome to the rrevolution,^700 \n we'll help work out what LEDs you need."],
         strings: [],
         typeSpeed: 5
     });
